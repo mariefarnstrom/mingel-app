9e3a8f69dabc9147db6816f152fa6d8e777870fa
Marie <fernstrm@hotmail.com>
2026-03-24 09:49:32 +0100

    Created more buttons and buttonrows

--- src/components/buttons/BaseButton.jsx
+++ src/components/buttons/BaseButton.jsx
@@ -3,19 +3,51 @@ import styled from '@emotion/styled';
 export const BaseButton = styled.button`
     background-color: var(--btn-primary);
     color: var(--btn-primary-text);
-
+    border: none;
     max-width: 100%;
     border-radius: 10px;
     font-family: var(--font-heading);
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    justify-content: center;
 `;
 
 export const StyledWideButton = styled(BaseButton)`
     height: 4.25rem;
     font-size: 1.5rem;
-    margin: 1rem;
+`;
+
+export const StyledSmallLightButton = styled(BaseButton)`
+    height: 4.25rem;
+    font-size: 1.5rem;
+    background-color: var(--btn-secondary);
+    color: var(--btn-secondary-text);
+    border: 2px solid var(--btn-secondary-text);
 `;
 
 export const StyledSmallButton = styled(BaseButton)`
     height: 4.25rem;
     font-size: 1.5rem;
+`;
+
+export const StyledSBigButton = styled(BaseButton)`
+    height: 7rem;
+    font-size: 3.5rem;
+`;
+
+export const StyledNewQuestionButton = styled(BaseButton)`
+    height: 7rem;
+    font-size: 1.5rem;
+    background-color: var(--btn-secondary);
+    color: var(--btn-secondary-text);
+    border: 2px solid var(--btn-secondary-text);
+`;
+
+export const StyledChooseProfileButton = styled(BaseButton)`
+    height: 4.5rem;
+    font-size: 1.25rem;
+    background-color: var(--btn-secondary);
+    color: var(--btn-secondary-text);
+    border: 2px solid var(--btn-secondary-text);
 `;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import { BaseButton, StyledWideButton, StyledSmallButton } from "../components/buttons/BaseButton";
+import { StyledWideButton, StyledSmallLightButton, StyledSmallButton, StyledSBigButton, StyledNewQuestionButton, StyledChooseProfileButton } from "../components/buttons/BaseButton";
 import { ButtonRow } from "../components/ButtonRow";
+import { ChooseProfileRow } from "../components/ChooseProfileRow";
 
 export default function Home() {
     return (
@@ -8,14 +9,36 @@ export default function Home() {
             <StyledWideButton>
                 SE SPELREGLER
             </StyledWideButton>
+
             <ButtonRow>
-                <StyledSmallButton>
+                <StyledSmallLightButton>
+                    <img src="backwardsArrow.svg" alt="back" />
                     TILLBAKA
-                </StyledSmallButton>
+                </StyledSmallLightButton>
                 <StyledSmallButton>
                     NÄSTA
+                    <img src="forwardArrow.svg" alt="forward" />
                 </StyledSmallButton>
             </ButtonRow>
+
+            <StyledSBigButton>
+                STUDENT
+            </StyledSBigButton>
+
+            <StyledNewQuestionButton>
+                GENERERA NY FRÅGA
+            </StyledNewQuestionButton>
+
+            <ChooseProfileRow>
+                <StyledChooseProfileButton>
+                    WEBBUTVECKLARE
+                </StyledChooseProfileButton>
+                <StyledChooseProfileButton>
+                    DIGITALDESIGNER
+                </StyledChooseProfileButton>
+            </ChooseProfileRow>
+
+
         </>
     );
 }
