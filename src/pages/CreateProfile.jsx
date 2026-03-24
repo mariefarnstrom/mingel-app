ec1e0342d306947ca0d669d9e3e9afbfe33cb6db
Marie <fernstrm@hotmail.com>
2026-03-24 12:50:18 +0100

    Placed components in pages

--- src/pages/CreateProfile.jsx
+++ src/pages/CreateProfile.jsx
@@ -1,7 +1,51 @@
+import { HeadingCard } from "../components/cards/CardHeading";
+import { ButtonRow } from "../components/buttons/ButtonRow";
+import { StyledSmallButton, StyledSmallLightButton } from "../components/buttons/Button";
+import { StyledOption, HiddenRadio } from "../components/buttons/FormButton";
+
 export default function CreateProfile() {
     return (
         <>
-            <h1>Create profile</h1>
+            <HeadingCard>
+                <h3>SKAPA DIN KARAKTÄR</h3>
+                <p>
+                    Skapa din avatar för att komma igång.
+                </p>
+            </HeadingCard>
+
+            <form action="">
+
+                <h3>NAMN</h3>
+                <input type="text" />
+
+                <h3>VÄLJ KLASS</h3>
+                <ButtonRow>
+                    <StyledOption>
+                        <input type="radio" name="profile" value="student" />
+                        <div>WEBBUTVECKLARE</div>
+                    </StyledOption>
+
+                    <StyledOption>
+                        <input type="radio" name="profile" value="teacher" />
+                        <div>DIGITALDESIGNER</div>
+                    </StyledOption>
+                </ButtonRow>
+
+                <h3>VÄLJ DIN AVATAR</h3>
+
+                <ButtonRow>
+                    <StyledSmallLightButton>
+                        <img src="backwardsArrow.svg" alt="back" />
+                        TILLBAKA
+                    </StyledSmallLightButton>
+                    <StyledSmallButton type="submit">
+                        KLAR
+                        <img src="forwardArrow.svg" alt="forward" />
+                    </StyledSmallButton>
+                </ButtonRow>
+
+            </form>
+
         </>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,27 @@
 import { StyledWideButton, StyledSmallLightButton, StyledSmallButton, StyledSBigButton, StyledNewQuestionButton, StyledChooseProfileButton } from "../components/buttons/Button";
+import { TextCard } from '../components/cards/CardText'
 import { ButtonRow } from "../components/buttons/ButtonRow";
 import { ChooseProfileRow } from "../components/buttons/ChooseProfileRow";
+import { GhostContainer } from "../components/GhostContainer";
 
 export default function Home() {
     return (
         <>
-            <h1>Home</h1>
+            <TextCard>
+                <h1>VÄLKOMMEN TILL YRGOXP</h1>
+                <p>Spelet som gör det enkelt att börja prata, Samla poäng genom att ställa frågor och vara aktiv i spelet.</p>
+                <p>Ju fler frågor du ställer, desto högre klättrar du på scoreboarden.</p>
+            </TextCard>
+
+            <GhostContainer>
+                <img src="ghost.svg" alt="Ghost image" />
+            </GhostContainer>
+
             <StyledWideButton>
                 SE SPELREGLER
             </StyledWideButton>
+
+
 
             <ButtonRow>
                 <StyledSmallLightButton>
