f303f5165be34d88aac8255822e1d780236b6200
mariatedeman <m.tedeman@hotmail.com>
2026-03-24 14:53:26 +0100

    Instructions page logic implemented. Hooks and data folders created related to instructions. Still to do: Step indicator

--- src/pages/Instructions.jsx
+++ src/pages/Instructions.jsx
@@ -1,9 +1,22 @@
+// Components
 import { StyledSmallLightButton, StyledSmallButton } from "../components/buttons/Button";
 import { ButtonRow } from "../components/buttons/ButtonRow";
 import { HeadingCard, BaseCard, InstructionsCard, BigImageCard } from "../components/cards/Cards"
 
+// Data
+import { useInstructions } from "../hooks/useInstructions";
 
 export default function Instructions() {
+
+    const { currentStep, 
+            stepNumber, 
+            totalSteps, 
+            lang,
+            nextStep, 
+            previousStep } = useInstructions();
+
+    if (!currentStep) return <p>Laddar...</p>;
+
     return (
         <>
         
@@ -14,24 +27,34 @@ export default function Instructions() {
         </HeadingCard>
 
         <BigImageCard>
-            <img src="../public/turtleWhite.svg" alt="" />
+            <img src={currentStep.img} alt="" />
         </BigImageCard>
 
         <InstructionsCard>
-            <h3>1. SKAPA PROFIL</h3>
-            <p>Välj namn och avatar för att komma igång.</p>
+            <h3>{stepNumber}. {currentStep[lang].step.toUpperCase()}</h3>
+            <p>{currentStep[lang].description}</p>
         </InstructionsCard>
 
         <BaseCard>
-            1/4
+            <section className="stepCountContainer">
+            {/* {Array.from({ length: totalSteps }).map((_, index) => (
+                <div 
+                    key={index}
+                    className={`${index + 1 === stepNumber ? 'active' : ''}`}>
+                </div>
+
+            ))} */}
+                
+            {stepNumber} / {totalSteps}
+            </section>
         </BaseCard>
     
             <ButtonRow>
-                <StyledSmallLightButton>
+                <StyledSmallLightButton onClick={previousStep}>
                     <img src="backwardsArrow.svg" alt="back" />
                     TILLBAKA
                 </StyledSmallLightButton>
-                <StyledSmallButton>
+                <StyledSmallButton onClick={nextStep}>
                     NÄSTA
                     <img src="forwardArrow.svg" alt="forward" />
                 </StyledSmallButton>
