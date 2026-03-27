8bc9561209e22f051e89a409a0c1a93ff1539ad3
mariatedeman <m.tedeman@hotmail.com>
2026-03-27 13:27:09 +0100

    stroke-width replaced with strokeWidth in all svg components, to follow JS standard

--- src/components/icons/Rocket.jsx
+++ src/components/icons/Rocket.jsx
@@ -8,34 +8,34 @@ export default function RocketIcon() {
         <title>business-product-startup-1</title>
        
         <g>
-            <path d="M180.9375 180.9375H200v9.5625h-19.0625Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M171.4375 190.5h9.5V200h-9.5Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="m171.4375 123.81249999999999 -9.5625 0 0 9.5 9.5625 0 0 9.5625 9.5 0 0 -47.625 -9.5 0 0 28.5625z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M152.375 180.9375h19.0625v9.5625h-19.0625Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M161.875 85.75h9.5625v9.5H161.875Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M133.3125 171.4375h19.0625v9.5h-19.0625Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="m142.875 133.3125 9.5 0 0 -9.5 9.5 0 0 -9.5 -9.5 0 0 -28.5625 9.5 0 0 -9.5625 -9.5 0 0 -28.5625 -9.5 0 0 85.6875z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M123.81249999999999 190.5h9.5V200h-9.5Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M123.81249999999999 114.3125h9.5v9.5h-9.5Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M123.81249999999999 95.25h9.5v9.5h-9.5Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M114.25 171.4375h9.5625v19.0625h-9.5625Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M85.6875 152.375h38.125v9.5625h-38.125Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M114.25 9.5h9.5625v9.5625h-9.5625Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M95.25 0h19v9.5h-19Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="m123.81249999999999 57.125 -9.5625 0 0 -9.5 -19 0 0 9.5 9.5 0 0 9.5625 -9.5 0 0 -9.5625 -9.5625 0 0 19.0625 9.5625 0 0 9.5625 19 0 0 -9.5625 9.5625 0 0 -19.0625z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M85.6875 171.4375h9.5625v19.0625h-9.5625Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="m66.625 142.875 9.5625 0 0 9.5 9.5 0 0 -9.5 38.125 0 0 9.5 9.5 0 0 -9.5 9.5625 0 0 -9.5625 -76.25 0 0 9.5625z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M85.6875 9.5h9.5625v9.5625h-9.5625Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M76.1875 190.5h9.5V200h-9.5Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M57.125 171.4375h19.0625v9.5H57.125Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="m85.6875 28.5625 0 -9.5 -9.5 0 0 9.5 -9.5625 0 0 19.0625 9.5625 0 0 -9.5 57.125 0 0 9.5 9.5625 0 0 -19.0625 -9.5625 0 0 -9.5 -9.5 0 0 9.5 -38.125 0z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="m66.625 47.625 -9.5 0 0 28.5625 -9.5 0 0 9.5625 9.5 0 0 28.5625 -9.5 0 0 9.5 9.5 0 0 9.5 9.5 0 0 -85.6875z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M38.0625 180.9375h19.0625v9.5625H38.0625Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M38.0625 85.75h9.5625v9.5H38.0625Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M28.5625 190.5h9.5V200H28.5625Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="m38.0625 95.25 -9.5 0 0 47.625 9.5 0 0 -9.5625 9.5625 0 0 -9.5 -9.5625 0 0 -28.5625z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M9.5 180.9375h19.0625v9.5625H9.5Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M0 190.5h9.5V200H0Z" fill="currentColor" stroke-width="6.25"></path>
+            <path d="M180.9375 180.9375H200v9.5625h-19.0625Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M171.4375 190.5h9.5V200h-9.5Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="m171.4375 123.81249999999999 -9.5625 0 0 9.5 9.5625 0 0 9.5625 9.5 0 0 -47.625 -9.5 0 0 28.5625z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M152.375 180.9375h19.0625v9.5625h-19.0625Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M161.875 85.75h9.5625v9.5H161.875Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M133.3125 171.4375h19.0625v9.5h-19.0625Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="m142.875 133.3125 9.5 0 0 -9.5 9.5 0 0 -9.5 -9.5 0 0 -28.5625 9.5 0 0 -9.5625 -9.5 0 0 -28.5625 -9.5 0 0 85.6875z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M123.81249999999999 190.5h9.5V200h-9.5Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M123.81249999999999 114.3125h9.5v9.5h-9.5Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M123.81249999999999 95.25h9.5v9.5h-9.5Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M114.25 171.4375h9.5625v19.0625h-9.5625Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M85.6875 152.375h38.125v9.5625h-38.125Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M114.25 9.5h9.5625v9.5625h-9.5625Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M95.25 0h19v9.5h-19Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="m123.81249999999999 57.125 -9.5625 0 0 -9.5 -19 0 0 9.5 9.5 0 0 9.5625 -9.5 0 0 -9.5625 -9.5625 0 0 19.0625 9.5625 0 0 9.5625 19 0 0 -9.5625 9.5625 0 0 -19.0625z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M85.6875 171.4375h9.5625v19.0625h-9.5625Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="m66.625 142.875 9.5625 0 0 9.5 9.5 0 0 -9.5 38.125 0 0 9.5 9.5 0 0 -9.5 9.5625 0 0 -9.5625 -76.25 0 0 9.5625z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M85.6875 9.5h9.5625v9.5625h-9.5625Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M76.1875 190.5h9.5V200h-9.5Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M57.125 171.4375h19.0625v9.5H57.125Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="m85.6875 28.5625 0 -9.5 -9.5 0 0 9.5 -9.5625 0 0 19.0625 9.5625 0 0 -9.5 57.125 0 0 9.5 9.5625 0 0 -19.0625 -9.5625 0 0 -9.5 -9.5 0 0 9.5 -38.125 0z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="m66.625 47.625 -9.5 0 0 28.5625 -9.5 0 0 9.5625 9.5 0 0 28.5625 -9.5 0 0 9.5 9.5 0 0 9.5 9.5 0 0 -85.6875z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M38.0625 180.9375h19.0625v9.5625H38.0625Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M38.0625 85.75h9.5625v9.5H38.0625Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M28.5625 190.5h9.5V200H28.5625Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="m38.0625 95.25 -9.5 0 0 47.625 9.5 0 0 -9.5625 9.5625 0 0 -9.5 -9.5625 0 0 -28.5625z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M9.5 180.9375h19.0625v9.5625H9.5Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M0 190.5h9.5V200H0Z" fill="currentColor" strokeWidth="6.25"></path>
         </g>
         </svg>
     )
--- src/components/icons/Star.jsx
+++ src/components/icons/Star.jsx
@@ -9,16 +9,16 @@ export default function StarIcon() {
         <title>social-rewards-rating-star-2</title>
 
         <g>
-            <path d="M190.5 66.625h-57.1875v9.5625h-9.5v9.5h-9.5v9.5h-9.5625V28.5625h9.5625V9.5h-9.5625V0h-9.5v9.5h-9.5625v19.0625h-9.5v19.0625h-9.5v19H9.5v9.5625H0v9.5h9.5v9.5h9.5625v9.5625H38.125v9.5h9.5v19.0625h9.5v-9.5h19.0625v-9.5625h19.0625v9.5625h-9.5625v9.5h-9.5v9.5h-9.5v9.5625H57.125v9.5H47.625v9.5625H38.125v9.5H28.5625v-9.5H19.0625v19h9.5V200h19.0625v-9.5625h9.5v-9.5h19.0625v-9.5h9.5V161.875h9.5625v-9.5h9.5v-28.5625h9.5625v9.5h9.5v9.5h9.5v9.5625h9.5625v9.5h9.5v9.5625h9.5v19h-9.5V200h19.0625v-9.5625h9.5v-19h-9.5v-19.0625H161.875v-19.0625h-9.5v-19.0625h9.5v-9.5h-19v9.5h-38.125v-9.5h28.5625v-9.5625h9.5625v-9.5h38.0625v9.5h9.5625v-9.5H200v-9.5h-9.5Zm-104.8125 38.125h-19v-9.5625H47.625v-9.5H28.5625v-9.5h47.625v9.5h9.5Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M161.875 95.1875h19.0625v9.5625H161.875Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M142.875 180.9375h9.5v9.5h-9.5Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M123.81249999999999 171.4375h19.0625v9.5h-19.0625Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M123.81249999999999 47.625h9.5v19h-9.5Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M114.3125 161.875h9.5v9.5625h-9.5Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M114.3125 28.5625h9.5v19.0625h-9.5Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M104.75000000000001 152.375h9.5625v9.5h-9.5625Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M38.125 133.3125h9.5v19.0625H38.125Z" fill="currentColor" stroke-width="6.25"></path>
-            <path d="M28.5625 152.375H38.125v19.0625H28.5625Z" fill="currentColor" stroke-width="6.25"></path>
+            <path d="M190.5 66.625h-57.1875v9.5625h-9.5v9.5h-9.5v9.5h-9.5625V28.5625h9.5625V9.5h-9.5625V0h-9.5v9.5h-9.5625v19.0625h-9.5v19.0625h-9.5v19H9.5v9.5625H0v9.5h9.5v9.5h9.5625v9.5625H38.125v9.5h9.5v19.0625h9.5v-9.5h19.0625v-9.5625h19.0625v9.5625h-9.5625v9.5h-9.5v9.5h-9.5v9.5625H57.125v9.5H47.625v9.5625H38.125v9.5H28.5625v-9.5H19.0625v19h9.5V200h19.0625v-9.5625h9.5v-9.5h19.0625v-9.5h9.5V161.875h9.5625v-9.5h9.5v-28.5625h9.5625v9.5h9.5v9.5h9.5v9.5625h9.5625v9.5h9.5v9.5625h9.5v19h-9.5V200h19.0625v-9.5625h9.5v-19h-9.5v-19.0625H161.875v-19.0625h-9.5v-19.0625h9.5v-9.5h-19v9.5h-38.125v-9.5h28.5625v-9.5625h9.5625v-9.5h38.0625v9.5h9.5625v-9.5H200v-9.5h-9.5Zm-104.8125 38.125h-19v-9.5625H47.625v-9.5H28.5625v-9.5h47.625v9.5h9.5Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M161.875 95.1875h19.0625v9.5625H161.875Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M142.875 180.9375h9.5v9.5h-9.5Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M123.81249999999999 171.4375h19.0625v9.5h-19.0625Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M123.81249999999999 47.625h9.5v19h-9.5Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M114.3125 161.875h9.5v9.5625h-9.5Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M114.3125 28.5625h9.5v19.0625h-9.5Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M104.75000000000001 152.375h9.5625v9.5h-9.5625Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M38.125 133.3125h9.5v19.0625H38.125Z" fill="currentColor" strokeWidth="6.25"></path>
+            <path d="M28.5625 152.375H38.125v19.0625H28.5625Z" fill="currentColor" strokeWidth="6.25"></path>
         </g>
         </svg>
     )
