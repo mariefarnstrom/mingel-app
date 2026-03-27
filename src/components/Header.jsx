ef82e9a4f919fe21cb3d5e493d1c16452dffcc6d
Marie <fernstrm@hotmail.com>
2026-03-27 14:03:35 +0100

    Added menu overlay with navigation and language toggle button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,44 @@
 import { useState } from "react"
 import { HamburgerMenu, StyledHeader } from "./Header.styles";
+import { GhostContainer } from "./GhostContainer";
+import { LanguageToggleDiv, MenuOverlay, StyledMenuLink } from "./MenuOverlay.styles";
+import LanguageToggle from "./LanguageToggle";
 
-export default function Header() {
+export default function Header({ lang, setLang }) {
 
     const [open, setOpen] = useState(false);
 
     return (
-        <StyledHeader>
-            <img src="/logo.svg" alt="" />
-            <HamburgerMenu onClick={() => setOpen(!open)}>
-                {open ? <img src='/x-icon.svg' alt='' /> : <img src='/hamburger-icon.svg' alt='' />}
-            </HamburgerMenu>
+        <>
+            <StyledHeader>
+                <img src="/logo.svg" alt="" />
+                <HamburgerMenu onClick={() => setOpen(!open)}>
+                    {open ? <img src='/x-icon.svg' alt='' /> : <img src='/hamburger-icon.svg' alt='' />}
+                </HamburgerMenu>
+            </StyledHeader>
 
-        </StyledHeader>
+            {open &&
+
+                <MenuOverlay>
+                    <nav>
+                        <StyledMenuLink to="/instructions" onClick={() => setOpen(false)}>REGLER</StyledMenuLink >
+                        <StyledMenuLink to="/score" onClick={() => setOpen(false)}>SCOREBOARD</StyledMenuLink >
+                        <StyledMenuLink to="/create-profile" onClick={() => setOpen(false)}>ÄNDRA PROFIL</StyledMenuLink >
+                    </nav>
+
+                    <GhostContainer>
+                        <img src="/ghost.svg" alt="Ghost" />
+                    </GhostContainer>
+
+                    <LanguageToggleDiv>
+                        <span>SWITCH TO ENGLISH</span>
+                        <LanguageToggle lang={lang} setLang={setLang} />
+
+                    </LanguageToggleDiv>
+
+                </MenuOverlay>
+            }
+        </>
     )
 
 }
--- src/components/Header.styles.jsx
+++ src/components/Header.styles.jsx
@@ -3,7 +3,9 @@ import styled from "@emotion/styled";
 export const StyledHeader = styled.header`
  display: flex;
  justify-content: space-between;
-`;
+ position: relative;
+ z-index: 10;
+`
 
 export const HamburgerMenu = styled.button`
 background: none;
