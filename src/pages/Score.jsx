847dfdcf5fb5d62406edcd5647c099d93db84476
Marie <fernstrm@hotmail.com>
2026-03-31 00:53:06 +0200

    Implemented live scores and sorted scoreboard with highest first

--- src/pages/Score.jsx
+++ src/pages/Score.jsx
@@ -1,35 +1,79 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../lib/supabaseClient";
+import { HeadingCard } from "../components/cards/Cards";
 
 export default function Score() {
 
     const [users, setUsers] = useState([]);
 
-    // Fetch scores from databse
+    // Fetch live scores from databse
     useEffect(() => {
-        const fetchData = async () => {
-            const { data, error } = await supabase
-                .from('users')
-                .select('*')
+        fetchUsers();
+        const channel = supabase
+            .channel("scoreboard")
+            .on(
+                "postgres_changes",
+                {
+                    event: "*",
+                    schema: "public",
+                    table: "users",
+                },
+                (payload) => {
+                    console.log("Change recieved!", payload);
 
-            if (error) {
-                console.error("Error fetching data:", error);
-            } else {
-                setUsers(data);
-            }
-        };
+                    setUsers((prev) => {
+                        let updated = [...prev];
 
-        fetchData();
+                        if (payload.eventType === "UPDATE") {
+                            updated = updated.map(u =>
+                                u.id === payload.new.id ? payload.new : u
+                            );
+                        }
 
+                        if (payload.eventType === "INSERT") {
+                            updated = [...updated, payload.new];
+                        }
+
+                        if (payload.eventType === "DELETE") {
+                            updated = updated.filter(u => u.id !== payload.old.id);
+                        }
+
+                        return updated.sort((a, b) => b.score - a.score);
+                    });
+                }
+            )
+            .subscribe();
+
+        return () =>
+            supabase.removeChannel(channel);
     }, []);
+
+    const fetchUsers = async () => {
+        const { data, error } = await supabase
+            .from('users')
+            .select('*')
+            .order("score", { ascending: false });
+
+        if (error) {
+            console.error("Error fetching data:", error);
+        } else {
+            setUsers(data);
+
+        }
+    };
 
 
     return (
         <>
-            <h1>Score</h1>
+            <HeadingCard>
+                <h3>SCOREBOARD</h3>
+                <p>Se vem som leder just nu!</p>
+            </HeadingCard>
 
-            { users.map((user) => (
-                <p key={user.id}>{user.name} - {user.score}</p>
+            {users.map((user, index) => (
+                <p key={user.id}>
+                    {index + 1}. {user.name} - {user.score}
+                </p>
 
             ))}
         </>
