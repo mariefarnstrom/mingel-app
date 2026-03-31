289b799accf6a258d011e36c393cb4d4791d2a83
Marie <fernstrm@hotmail.com>
2026-04-01 00:05:08 +0200

    Created useScore hook

--- src/pages/Score.jsx
+++ src/pages/Score.jsx
@@ -1,67 +1,10 @@
-import { useEffect, useState } from "react";
-import { supabase } from "../lib/supabaseClient";
 import { HeadingCard } from "../components/cards/Cards";
+import { useScore } from "../hooks/useScore";
 
 export default function Score() {
+    const { users, loading, error } = useScore();
 
-    const [users, setUsers] = useState([]);
-
-    // Fetch live scores from databse
-    useEffect(() => {
-        fetchUsers();
-        const channel = supabase
-            .channel("scoreboard")
-            .on(
-                "postgres_changes",
-                {
-                    event: "*",
-                    schema: "public",
-                    table: "users",
-                },
-                (payload) => {
-                    console.log("Change recieved!", payload);
-
-                    setUsers((prev) => {
-                        let updated = [...prev];
-
-                        if (payload.eventType === "UPDATE") {
-                            updated = updated.map(u =>
-                                u.id === payload.new.id ? payload.new : u
-                            );
-                        }
-
-                        if (payload.eventType === "INSERT") {
-                            updated = [...updated, payload.new];
-                        }
-
-                        if (payload.eventType === "DELETE") {
-                            updated = updated.filter(u => u.id !== payload.old.id);
-                        }
-
-                        return updated.sort((a, b) => b.score - a.score);
-                    });
-                }
-            )
-            .subscribe();
-
-        return () =>
-            supabase.removeChannel(channel);
-    }, []);
-
-    const fetchUsers = async () => {
-        const { data, error } = await supabase
-            .from('users')
-            .select('*')
-            .order("score", { ascending: false });
-
-        if (error) {
-            console.error("Error fetching data:", error);
-        } else {
-            setUsers(data);
-
-        }
-    };
-
+    if (loading) return <p>Laddar scoreboard...</p>;
 
     return (
         <>
@@ -78,4 +21,5 @@ export default function Score() {
             ))}
         </>
     );
+
 }
