00a6231e9eb2cbb6a58a2c07e19d5982c6f2d0f9
mariatedeman <m.tedeman@hotmail.com>
2026-04-08 15:31:24 +0200

    Database updated with english questions, logic implemented

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -52,7 +52,7 @@ export default function Questions() {
             </HeadingCard>
 
             <QuestionCard>
-                <p>{questions[currentId].question}</p>
+                <p>{questions[currentId][`question_${lang}`]}</p>
             </QuestionCard>
 
             <NewQuestionButton onClick={setRandomQuestion}>
