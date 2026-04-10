5b14339f0460343c21334af2f832baffb0230f04
Marie <fernstrm@hotmail.com>
2026-04-11 00:36:44 +0200

    Added intro video

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import { LanguageProvider } from './contexts/LanguageContext';
+import { useState, useEffect } from 'react';
 
 // Pages
 import Home from './pages/Home'
@@ -14,27 +15,51 @@ import FinishedProfile from './pages/FinishedProfile'
 // Components
 import Header from './components/Header'
 import { ColorModeProvider } from './contexts/ColorModeContext';
+import { IntroVideo, VideoWrapper } from './App.styles';
 
 
 function App() {
+  const [showIntro, setShowIntro] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowIntro(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (showIntro) {
+    return (
+      <VideoWrapper>
+        <IntroVideo
+          autoPlay
+          muted
+          playsInline
+          onEnded={() => setShowIntro(false)}
+        >
+          <source src="/intro.mp4" type="video/mp4" />
+        </IntroVideo>
+      </VideoWrapper>
+    )
+  }
 
   return (
     <>
       <BrowserRouter>
-      <ColorModeProvider>
-        <LanguageProvider>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/instructions" element={<Instructions />} />
-            <Route path="/questions/:level" element={<Questions />} />
-            <Route path="/score" element={<ScoreBoard />} />
-            <Route path="/create-profile" element={<CreateProfile />} />
-            <Route path="/choose-difficulty" element={<ChooseDifficulty />} />
-            <Route path="/finished-profile" element={<FinishedProfile />} />
-          </Routes>
-        </LanguageProvider>
-      </ColorModeProvider>
+        <ColorModeProvider>
+          <LanguageProvider>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/instructions" element={<Instructions />} />
+              <Route path="/questions/:level" element={<Questions />} />
+              <Route path="/score" element={<ScoreBoard />} />
+              <Route path="/create-profile" element={<CreateProfile />} />
+              <Route path="/choose-difficulty" element={<ChooseDifficulty />} />
+              <Route path="/finished-profile" element={<FinishedProfile />} />
+            </Routes>
+          </LanguageProvider>
+        </ColorModeProvider>
       </BrowserRouter>
 
     </>
